ee2b379b19b20866924f38cf425d50070b341bb5
anotherbuginthecode <mangone.devops@gmail.com>
2022-09-29 11:31:23 +0200

    [add] keypair resource to bastion host to use personal keypair insted one created on aws

--- modules/aws/bastion/key.tf
+++ modules/aws/bastion/key.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "mykeypair" {
-  count = var.path_to_public_key ? 1 : null
+  count = var.path_to_public_key == null ? 0 : 1
   key_name   = var.keypair_name
   public_key = file(var.path_to_public_key)
 }
