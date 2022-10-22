3b6a79aa6c588c26972abbdc3a25f1e7627d9e35
anotherbuginthecode <mangone.devops@gmail.com>
2022-10-22 16:37:30 +0200

    [add] ecs autoscaling modules

---
+++ modules/aws/ecs-autoscaling-ec2-spot/instances.tf
@@ -0,0 +1,21 @@
+# get the most up-to-date AWS EC2 AMI that is ECS optimized.
+data "aws_ami" "aws_optimized_ecs" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami*amazon-ecs-optimized"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["591542846629"] # Canonical
+}
