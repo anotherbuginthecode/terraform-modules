f27cd91500d141c29b36f3a9b8b08d297037071c
anotherbuginthecode <mangone.devops@gmail.com>
2022-10-23 12:27:13 +0200

    [add] ecs-autoscaling-policy module

--- modules/aws/ecs-autoscaling-policy/autoscaling_policy.tf
+++ modules/aws/ecs-autoscaling-policy/autoscaling_policy.tf
@@ -1,6 +1,6 @@
-resource "aws_autoscaling_policy" "ecs_cpu_policy" {
-  count = var.enable_autoscaling_on_cpu ? 1 : 0
-  name = "${var.cluster_name}-${var.cluster_environment}-cpu-policy"
+resource "aws_autoscaling_policy" "ecs_cpu_scale_out_policy" {
+  count = var.enable_scale_out_on_cpu ? 1 : 0
+  name = "${var.cluster_name}-${var.cluster_environment}-cpu-scale-out-policy"
   autoscaling_group_name = var.autoscaling_group_name
   adjustment_type = "ChangeInCapacity"
   scaling_adjustment = "1" # +1
