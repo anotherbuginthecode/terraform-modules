95e5c923b122e172f38f031b8da0e80a5ca12236
anotherbuginthecode <mangone.devops@gmail.com>
2022-10-24 10:32:58 +0200

    [fix]

--- modules/aws/ecs-lb/targetgroup.tf
+++ modules/aws/ecs-lb/targetgroup.tf
@@ -4,7 +4,7 @@ resource "aws_lb_target_group" "lb_target_group" {
   port        = "80"
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
-  target_type = "ip"
+  target_type = "instance"
 
   health_check {
     healthy_threshold   = "3"
