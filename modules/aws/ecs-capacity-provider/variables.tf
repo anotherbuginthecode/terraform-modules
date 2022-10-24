19ae3d261940a193a8e4a22e15611d2451c6f7f6
anotherbuginthecode <mangone.devops@gmail.com>
2022-10-24 13:13:33 +0200

    [add] ecs capacity provider module

---
+++ modules/aws/ecs-capacity-provider/variables.tf
@@ -0,0 +1,7 @@
+variable "auto_scaling_group_arn" {
+  type = string
+  default = ""
+}
+
+variable "cluster_name" {
+}
--- modules/aws/ecs-cluster/ecs.tf
+++ modules/aws/ecs-cluster/ecs.tf
@@ -8,17 +8,3 @@ resource "aws_ecs_cluster" "ecs_cluster" {
     Name        = "${var.cluster_name}"
   }
 }
-
-resource "aws_ecs_capacity_provider" "ecs_cluster" {
-  count = var.auto_scaling_group_arn != "" ? 1 : 0
-  name = "capacity-provider-${var.cluster_name}"
-  auto_scaling_group_provider {
-    auto_scaling_group_arn         = var.auto_scaling_group_arn
-    managed_termination_protection = "DISABLED"
-
-    managed_scaling {
-      status          = "ENABLED"
-      target_capacity = 85
-    }
-  }
-}
