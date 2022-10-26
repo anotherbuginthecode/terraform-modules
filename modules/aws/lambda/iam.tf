9fd145ae6fe4dd08acd7fac30040d7fc72ae5ea9
anotherbuginthecode <mangone.devops@gmail.com>
2022-10-26 15:16:46 +0200

    [fix] role value

--- modules/aws/lambda/iam.tf
+++ modules/aws/lambda/iam.tf
@@ -1,6 +1,6 @@
 # define lambda role
 resource "aws_iam_role" "lambda_exec" {
-  name = replace("${var.function_name}_role","-","_")
+  name = "${var.function_name}-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -25,12 +25,12 @@ resource "aws_iam_policy" "policy" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_basic" {
-  role = aws_iam_role.lambda_exec.arn
+  role = aws_iam_role.lambda_exec.name
   policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_custom" {
   count = var.create_policy ? 1 : 0
-  role = aws_iam_role.lambda_exec.arn
+  role = aws_iam_role.lambda_exec.name
   policy_arn  = aws_iam_policy.policy[count.index].arn
 }
