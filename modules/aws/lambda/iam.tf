dbd293a9dd02aebc8172e8df7f3e429244cd3bfe
anotherbuginthecode <mangone.devops@gmail.com>
2022-10-26 14:51:34 +0200

    [refactor] define role inside module + remove unused variables

--- modules/aws/lambda/iam.tf
+++ modules/aws/lambda/iam.tf
@@ -1,21 +1,36 @@
+# define lambda role
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.function_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
 # define lambda policy
 resource "aws_iam_policy" "policy" {
   count = var.create_policy ? 1 : 0
-
   name  = var.policy_name
   description = var.policy_description
-
   policy = var.policy
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_basic" {
-  role = var.role_name
+  role = aws_iam_role.lambda_exec.arn
   policy_arn  = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_custom" {
   count = var.create_policy ? 1 : 0
-
-  role = var.role_name
+  role = aws_iam_role.lambda_exec.arn
   policy_arn  = aws_iam_policy.policy[count.index].arn
 }
