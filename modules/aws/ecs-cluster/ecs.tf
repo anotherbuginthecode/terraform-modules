e28e384bbf0abc7be6391a39a2e89b259dd438e0
anotherbuginthecode <mangone.devops@gmail.com>
2022-11-02 10:43:36 +0100

    [update] remove capacity_provider

--- modules/aws/ecs-cluster/ecs.tf
+++ modules/aws/ecs-cluster/ecs.tf
@@ -1,20 +1,6 @@
 resource "aws_ecs_cluster" "cluster" {
   name               = var.cluster_name
-  capacity_providers = [aws_ecs_capacity_provider.cluster.name]
   tags = {
     "Terraform"       = "true"
-  }
-}
-
-resource "aws_ecs_capacity_provider" "cluster" {
-  name = "${var.cluster_name}-capacity-provider"
-  auto_scaling_group_provider {
-    auto_scaling_group_arn         = var.asg_arn
-    managed_termination_protection = "DISABLED"
-
-    # managed_scaling {
-    #   status          = "ENABLED"
-    #   target_capacity = 85
-    # }
   }
 }
--- modules/aws/ecs-cluster/variables.tf
+++ modules/aws/ecs-cluster/variables.tf
@@ -1,7 +1,3 @@
 variable "cluster_name" {
   type = string
 }
-
-variable "asg_arn" {
-  type = string
-}
