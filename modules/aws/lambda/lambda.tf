bbe0aaf39bff2d86f581796ff8b8d41484cbda7a
anotherbuginthecode <mangone.devops@gmail.com>
2022-11-16 12:11:15 +0100

    fix typos

--- modules/aws/lambda/lambda.tf
+++ modules/aws/lambda/lambda.tf
@@ -13,7 +13,7 @@ resource "aws_lambda_function" "lambda" {
   memory_size = var.memory_size
 
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  role = var.create_role ?  aws_iam_role.lambda_exec.arn : var.role_arn
+  role = var.create_role ?  aws_iam_role.lambda_exec[0].arn : var.role_arn
 
   layers = var.layers
   
