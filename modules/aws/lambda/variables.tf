3319133c14f3e4ba8e36c247583d442e13ec9440
anotherbuginthecode <mangone.devops@gmail.com>
2022-11-16 12:21:14 +0100

    restore list(map(string)) for environment_variables

--- modules/aws/lambda/variables.tf
+++ modules/aws/lambda/variables.tf
@@ -46,9 +46,9 @@ variable "memory_size" {
 }
 
 variable "environment_variables" {
-  type = map(string)
+  type = list(map(string))
   description  = "(Optional) Environmente variables to add to the lambda."
-  default = {}
+  default = []
 }
 
 variable "create_policy" {
