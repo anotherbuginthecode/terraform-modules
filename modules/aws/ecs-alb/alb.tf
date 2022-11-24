8476254a62e56ee52cb96524b9f291ae845e8a68
anotherbuginthecode <mangone.devops@gmail.com>
2022-11-24 15:23:42 +0100

    fix alias zone_id

--- modules/aws/ecs-alb/alb.tf
+++ modules/aws/ecs-alb/alb.tf
@@ -95,7 +95,7 @@ resource "aws_route53_record" "record-lb" {
 
   alias {
     name = aws_lb.lb.dns_name
-    zone_id = "${data.aws_route53_zone.zone[0].id}"
+    zone_id = aws_lb.lb.zone_id
     evaluate_target_health = true
   }
 
